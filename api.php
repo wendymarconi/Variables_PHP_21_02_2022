7efb034183c790f325d9c54df867a4cf5ec76751
wendymarconi <wendymarconiquintero28@gmail.com>
2022-02-28 13:33:30 -0500

    variables

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+    $nomUsu = (String) "Wendy Johanna Marconi Quintero";
+    $edaUsu = (int) 30 ;
+    $altUsu = (float) 1.67;
+
+    $HobUsu = (array)[
+        (string) "Escuchar Musica",
+        (string) "Mirar Series",
+        (string) "Viajar"
+    ];
+
+    echo "";
+?> 
