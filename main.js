268552abbb1013eb58826d9058b063d08c5c86f7
wendymarconi <wendymarconiquintero28@gmail.com>
2022-03-09 20:31:22 -0500

    Variables

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 addEventListener("DOMContentLoaded", async(e)=>{
     let peticion  = await fetch("https://wendymarconi.000webhostapp.com/Variables_PHP_21_02_2022/api.php");
-    let texto = await peticion.json();
+    let texto = await peticion.text();
     document.body.insertAdjacentText("afterend",texto);
 })
 
